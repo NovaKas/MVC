0457238dc52b789f99b785d8ba4d670b6ec216a7
Kasia Nowak <k.nowak22@o2.pl>
2017-01-22 14:45:00 +0100

    Add project files.

---
+++ WebApplicationLogin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationLogin.Startup))]
+namespace WebApplicationLogin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
