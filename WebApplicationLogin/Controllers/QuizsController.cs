d617d20b4dfde52faf4b0aa5956813cba0f2c452
knowak22 <k.nowak22@o2.pl>
2017-01-22 22:43:09 +0100

    Quiz Controller dodawanie, usuwanie, edycja, wyswietlanie listy, mapowanie na viewmodel QuizViewModel

--- WebApplicationLogin/Controllers/QuizsController.cs
+++ WebApplicationLogin/Controllers/QuizsController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using WebApplicationLogin.Models;
+using WebApplicationLogin.Models.ViewModels;
 
 namespace WebApplicationLogin.Controllers
 {
@@ -17,7 +18,13 @@ namespace WebApplicationLogin.Controllers
         // GET: Quizs
         public ActionResult Index()
         {
-            return View(db.Quizs.ToList());
+            var model = db.Quizs.Select(x => new QuizViewModel
+            {
+                Id = x.QuizID,
+                Name = x.Name,
+                Timer = x.Timer
+            });
+            return View(model);
         }
 
         // GET: Quizs/Details/5
@@ -27,12 +34,21 @@ namespace WebApplicationLogin.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
             Quiz quiz = db.Quizs.Find(id);
             if (quiz == null)
             {
                 return HttpNotFound();
             }
-            return View(quiz);
+
+            var model = new QuizViewModel
+            {
+                Id = quiz.QuizID,
+                Name = quiz.Name,
+                Timer = quiz.Timer
+            };
+
+            return View(model);
         }
 
         // GET: Quizs/Create
@@ -46,11 +62,16 @@ namespace WebApplicationLogin.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "QuizID,Name,Timer")] Quiz quiz)
+        public ActionResult Create([Bind(Include = "QuizID,Name,Timer")] QuizViewModel quiz)
         {
             if (ModelState.IsValid)
             {
-                db.Quizs.Add(quiz);
+                var model = new Quiz
+                {
+                    Name = quiz.Name,
+                    QuizID = quiz.Timer
+                };
+                db.Quizs.Add(model);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
@@ -70,7 +91,15 @@ namespace WebApplicationLogin.Controllers
             {
                 return HttpNotFound();
             }
-            return View(quiz);
+
+            var model = new QuizViewModel
+            {
+                Id = quiz.QuizID,
+                Name = quiz.Name,
+                Timer = quiz.Timer
+            };
+
+            return View(model);
         }
 
         // POST: Quizs/Edit/5
@@ -78,12 +107,22 @@ namespace WebApplicationLogin.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "QuizID,Name,Timer")] Quiz quiz)
+        public ActionResult Edit([Bind(Include = "QuizID,Name,Timer")] QuizViewModel quiz)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(quiz).State = EntityState.Modified;
+                var model = db.Quizs.SingleOrDefault(x => x.QuizID == quiz.Id);
+                if (model.Name != quiz.Name)
+                {
+                    model.Name = quiz.Name;
+                }
+                if(model.Timer != quiz.Timer)
+                {
+                    model.Timer = quiz.Timer;
+                }
+
                 db.SaveChanges();
+
                 return RedirectToAction("Index");
             }
             return View(quiz);
@@ -101,7 +140,15 @@ namespace WebApplicationLogin.Controllers
             {
                 return HttpNotFound();
             }
-            return View(quiz);
+
+            var model = new QuizViewModel
+            {
+                Id = quiz.QuizID,
+                Name = quiz.Name,
+                Timer = quiz.Timer
+            };
+
+            return View(model);
         }
 
         // POST: Quizs/Delete/5
---
+++ WebApplicationLogin/Models/ViewModels/QuizViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationLogin.Models.ViewModels
+{
+    public class QuizViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Timer { get; set; }
+    }
+}
